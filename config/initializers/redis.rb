7e0c3dec2a62526731fa016392282036302fbac2
leevic31 <leevic31@gmail.com>
2025-03-15 15:32:28 -0400

    reconfigure redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,4 @@
-$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+# $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+
+url = ENV["REDIS_URL"]
+$redis = Redis.new(url: url, ssl: true)
