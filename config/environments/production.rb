15495a2c2f0a5bff7f66b5fc7163d0d2ed3c0d13
leevic31 <leevic31@gmail.com>
2025-03-16 10:39:53 -0400

    Configure production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  
+  config.action_cable.disable_request_forgery_protection = true
   config.action_cable.url = "wss://message-me-a4a8521c1c9b.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = ['https://message-me-a4a8521c1c9b.herokuapp.com']
 end
